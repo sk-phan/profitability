6c58c4e864f713b4cc84797169fed486db373bd2
Suki Nhung Phan <nhungphan@Joel-Lov.local>
2022-06-05 22:58:51 +0300

    added test solutions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import FormCaculator from './components/FormCaculator';
+import styled from 'styled-components'
+
+const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    padding: 4rem;
+`
+
+const Logo = styled.img`
+    width: 20rem;
+
+    @media (max-width: 650px) {
+      margin-top: 4rem;
+
+    }
+`
 
 function App() {
   return (
-    <div className="App">
+    <Container>
+      <Logo src='https://diffosolutions.com/wp-content/uploads/2021/11/diffo-platform-logo.png' alt='Diffo Solution Logo' />
       <FormCaculator />
-    </div>
+    </Container>
   );
 }
 
--- my-app/src/components/FormCaculator.js
+++ my-app/src/components/FormCaculator.js
@@ -2,6 +2,20 @@ import {useState} from 'react'
 import styled from 'styled-components'
 
 
+//Testing
+// 1. Should show initialstate correctly (all input values and results should equal to 0)
+// 2. Should replace the initial state with input numbers
+// 3. Should not be able to input string, accept only numbers
+// 4. Should not change display when the caculation result appears
+// 5. The income result should equal the input income
+// 6. Total time based cost = price per hour * hours (15 * 2 = 30)
+// 7. Total distance based cost = price per km * km (20 * 2 = 40)
+// 8. Profitability = income - (total based cost + distance based cost)
+// 8a.Profitability caculation example:  2000 - (120 + 200) = 1680
+// 9. If the profit value is a interger number, it should ground to 2 digits (2384.9339393 = 2384.93)
+
+
+
 const FormCaculator = () => {
 
     const [distance, setDistance] = useState(0);
@@ -16,56 +30,70 @@ const FormCaculator = () => {
         <Form>
             <BoardContainer>
                 <Label htmlFor='income'>
-                    Income (€)
+                    Income ($)
                     <input 
                         id='income'
                         type='number'
                         value={income}
-                        onChange={(e) => setIncome(e.target.value)}
+                        min='0'
+                        onChange={(e) => setIncome((e.target.value))}
                     />
                 </Label>
-                <Label htmlFor='distance'>
-                    Distance (km)
-                    <input 
-                        id='distance'
-                        type='number'
-                        value={distance}
-                        onChange={(e) => setDistance(e.target.value)}
-                    /> 
-                </Label>
-                <Label htmlFor='price-per-km'>
-                    Price (per km)
-                    <input 
-                        id='price-per-km'
-                        type='number'
-                        value={pricePerKm}
-                        onChange={(e) => setPricePerKm(e.target.value)}
-                    />
-                </Label>
-                <Label htmlFor='hours'>
-                    Time (hours)
-                    <input 
-                        id='hours'
-                        type='number'
-                        value={hours}
-                        onChange={(e) => setHours(e.target.value)}
-                    />
-                </Label>
-                <Label htmlFor='price-per-hour'>
-                    Price (per hour)
-                    <input 
-                        id='price-per-hour'
-                        type='number'
-                        value={pricePerHour}
-                        onChange={(e) => setPricePerHour(e.target.value)}
-                    />
-                </Label>
+                <LabelContainer>
+                    <Label htmlFor='distance'>
+                        Distance (km)
+                        <input 
+                            id='distance'
+                            type='number'
+                            value={distance}
+                            min='0'
+                            onChange={(e) => setDistance(e.target.value)}
+                        /> 
+                    </Label>
+                    <Label htmlFor='price-per-km'>
+                        Price ($/km)
+                        <input 
+                            id='price-per-km'
+                            type='number'
+                            value={pricePerKm}
+                            min='0'
+                            onChange={(e) => setPricePerKm(e.target.value)}
+                        />
+                    </Label>
+                </LabelContainer>
+                <LabelContainer>
+
+                    <Label htmlFor='hours'>
+                        Time (hours)
+                        <input 
+                            id='hours'
+                            type='number'
+                            value={hours}
+                            min='0'
+                            onChange={(e) => setHours(e.target.value)}
+                        />
+                    </Label>
+                    <Label htmlFor='price-per-hour'>
+                        Price ($/hour)
+                        <input 
+                            id='price-per-hour'
+                            type='number'
+                            value={pricePerHour}
+                            min='0'
+                            onChange={(e) => setPricePerHour(e.target.value)}
+                        />
+                    </Label>
+                </LabelContainer>
             </BoardContainer>
             <ResultContainer>
-                <ResultDetail>Income: {income}</ResultDetail>
-                <ResultDetail>Total distance based cost: {distance*pricePerKm}</ResultDetail>
-                <ResultDetail>Total hour based cost: {hours*pricePerHour}</ResultDetail>
-                <ResultDetail>Profitability: {profitability}</ResultDetail>
+                <Data>
+                    <ResultDetail>Income: <span>{income} $</span></ResultDetail>
+                    <ResultDetail>Total distance based cost: <span>{(distance*pricePerKm)} $</span></ResultDetail>
+                    <ResultDetail>Total time based cost: <span>{hours*pricePerHour} $</span></ResultDetail>
+                </Data>
+                <Profit>
+                    <ProfitResult>Profitability: <span>{profitability.toFixed(2)} $</span></ProfitResult>
+                </Profit>
             </ResultContainer>
         </Form>
     )
@@ -76,43 +104,104 @@ export default FormCaculator
 const Form = styled.form`
     display: flex;
     flex-direction: row;
-    align-items: center;
-    background-color: #eee;
     gap: 2rem;
+    width: 1200px;
     
+    
+    @media (max-width: 650px) {
+        flex-direction: column;
+        width: 37.5rem;
+        height: 100vh;
+    }
 `
 
 const BoardContainer = styled.div`
+    background-color: #fff;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    justify-content: center;
+    width: 55%;
+    height: 400px;
+    padding: 4rem;
+    box-shadow: rgba(0, 0, 0, 0.05) 1.95px 1.95px 2.6px;
+
+    @media (max-width: 650px) {
+        width: 30rem;
+        height: 600px;
+
+    }
+`
+const LabelContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 5rem;
+
+    @media (max-width: 650px) {
+        flex-direction: column;
+        gap: 2rem;
+    }
 `
 
 const Label = styled.label`
     font-size: 1.8rem;
-    font-weight: bold;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     gap: 1rem;
     margin-bottom: 2rem;
 
+
     input {
         border: none;
-        padding: 1rem;
-        width: 20rem;
-        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+        border-radius: 5px;
+        padding: 1.2rem;
+        width: 28rem;
+        height: 5rem;
+        font-size: 1.6rem;
+        box-sizing: border-box;
+        background-color: #f5f5f5;
     }
     
 `
 
-const ResultContainer = styled(BoardContainer)`
-   gap: 8.4brem;
-   align-self: flex-start;
+const ResultContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+   width: 30%;
+
+   @media (max-width: 650px) {
+        width: 30rem;
+    }
+
+   
+   `
+const Data = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 4rem;
+    width: 100%;
+    height: 200px;
+    background-color: #fff;
+
 `
+
+const Profit = styled(Data)`
+    height: 10rem;
+    
+    `
 const ResultDetail = styled.p`
     font-size: 1.8rem;
-    margin: 0;
-    text-align: left;
-
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`
+const ProfitResult = styled(ResultDetail)`
+    font-size: 2rem;
+    font-weight: bold;
+    letter-spacing: 1px;
+    
+    @media (max-width: 650px) {
+        font-size: 1.8rem;
+    }
 `
